33574945bc31fef9122b470494fe243b8135f34d
Ryan Block <Ryan.Block93@gmail.com>
2020-03-15 16:31:54 -0400

    updated objects to not be strings since it's a js file

--- index.js
+++ index.js
@@ -1,70 +1,70 @@
 module.exports = {
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
-  "extends": [
-    "airbnb-base",
-    "prettier",
-    "prettier/@typescript-eslint",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:jest/recommended",
-    "plugin:prettier/recommended"
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended'
   ],
-  "env": {
-    "commonjs": true,
-    "es6": true,
-    "node": true,
-    "jest/globals": true
+  env: {
+    'commonjs': true,
+    'es6': true,
+    'node': true,
+    'jest/globals': true
   },
-  "rules": {
-    "prettier/prettier": [
-      "error",
+  rules: {
+    'prettier/prettier': [
+      'error',
       {
-        "singleQuote": true
+        singleQuote: true
       }
     ],
-    "no-underscore-dangle": "off",
-    "object-shorthand": [
-      "error",
-      "always"
+    'no-underscore-dangle': 'off',
+    'object-shorthand': [
+      'error',
+      'always'
     ],
-    "func-names": [
-      "error",
-      "always"
+    'func-names': [
+      'error',
+      'always'
     ],
-    "no-var": "error",
-    "no-console": "warn",
-    "no-useless-constructor": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "import/no-extraneous-dependencies": "warn",
-    "class-methods-use-this": "warn",
-    "jest/no-focused-tests": "warn",
-    "no-unused-vars": [
-      "error",
+    'no-var': 'error',
+    'no-console': 'warn',
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'import/no-extraneous-dependencies': 'warn',
+    'class-methods-use-this': 'warn',
+    'jest/no-focused-tests': 'warn',
+    'no-unused-vars': [
+      'error',
       {
-        "vars": "all",
-        "args": "none"
+        vars: 'all',
+        args: 'none'
       }
     ],
-    "import/prefer-default-export": "off",
-    "import/extensions": [
-      "error",
-      "ignorePackages",
+    'import/prefer-default-export': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
       {
-        "js": "never",
-        "ts": "never"
+        js: 'never',
+        ts: 'never'
       }
     ]
   },
-  "settings": {
-    "import/resolver": {
-      "node": {
-        "extensions": [
-          ".js",
-          ".ts"
+  settings: {
+    'import/resolver': {
+      node: {
+        'extensions': [
+          '.js',
+          '.ts'
         ]
       }
     }
